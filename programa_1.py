e27989e8dfd4fa8c81fcc60c437e7ec2ed887364
Pedroitamaro <itamaropedro@gmail.com>
2022-11-17 20:58:17 -0300

    Adições: Laço de repetição While, input para selecionar o numero de tentativas para acertar o numero e um contador de tentativas restante.

--- programa_1.py
+++ programa_1.py
@@ -1,27 +1,38 @@
 print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
-print("*********************************")
+print("*********************************\n")
 
 numero_secreto = 42
 
-chute_str = input("digite o seu número: ")
+tentativas_str = (input ("\nEm quantas tentativas você quer tentar acertar: "))
+tentativas = int (tentativas_str)
 
-chute = int(chute_str)
+while (tentativas > 0):
+    
+    if (tentativas>0):
+        print ("Você tem", tentativas, "tentativas restantes")
+    
 
-acertou = chute == numero_secreto
+    chute_str = input ("\ndigite o seu número: ")
 
-maior = chute>numero_secreto
+    chute = int (chute_str)
+    acertou = chute == numero_secreto
+    
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-menor = chute<numero_secreto
+    if (acertou):
+        print ("Você acertou!\n")
+        print ("Fim de jogo!\n")
+    else:
+        if (maior):
+            tentativas -= 1
+            print ("\nVocê errou! Seu chute foi um valor mais alto que o número secreto.\n")
+        elif (menor):
+            tentativas -= 1
+            print ("Você errou! Seu chute foi um valor mais baixo que o número secreto.\n")
 
-if (acertou):
-    print("Você acertou!")
-
-else:
-    if (maior):
-        print("Você errou! Seu chute foi um valor mais alto que o número secreto.")
-    elif (menor):
-        print("Você errou! Seu chute foi um valor mais baixo que o número secreto.")
-
-print("fim de jogo.")
+if (tentativas == 0):
+    print ("Suas tentativas acabaram.\n")
+    print ("Fim de jogo!\n")
 
