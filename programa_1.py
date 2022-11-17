7d0a876d83af36a9e52b00259f21a9755f44be78
Pedroitamaro <itamaropedro@gmail.com>
2022-11-17 19:51:24 -0300

    programa_1.py

--- programa_1.py
+++ programa_1.py
@@ -1,1 +1,27 @@
-print ("olá mundo")
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_str = input("digite o seu número: ")
+
+chute = int(chute_str)
+
+acertou = chute == numero_secreto
+
+maior = chute>numero_secreto
+
+menor = chute<numero_secreto
+
+if (acertou):
+    print("Você acertou!")
+
+else:
+    if (maior):
+        print("Você errou! Seu chute foi um valor mais alto que o número secreto.")
+    elif (menor):
+        print("Você errou! Seu chute foi um valor mais baixo que o número secreto.")
+
+print("fim de jogo.")
+
