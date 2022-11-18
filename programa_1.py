3679d3a78563883c7d3ace862618846811a5b646
Pedroitamaro <itamaropedro@gmail.com>
2022-11-17 22:15:36 -0300

    Adicionado verificador para quantidade de tentativas zerado ou negativa; adicionado novas mensagem de derrota;

--- programa_1.py
+++ programa_1.py
@@ -4,35 +4,53 @@ print("*********************************\n")
 
 numero_secreto = 42
 
-tentativas_str = (input ("\nEm quantas tentativas você quer tentar acertar: "))
+
+
+tentativas_str = (input ("\nEm quantas tentativas você quer tentar acertar: ",))
 tentativas = int (tentativas_str)
+
+while (tentativas <= 0):
+    print ("Você precisa colocar um número maior que 0.")
+    tentativas_str = (input ("\nEm quantas tentativas você quer tentar acertar: ",))
+    tentativas = int (tentativas_str)
+    
 
 while (tentativas > 0):
     
-    if (tentativas>0):
-        print ("Você tem", tentativas, "tentativas restantes")
+    if (tentativas>1):
+        
+        print ("Você tem", tentativas, "chances.")
+    elif (tentativas == 1 and chute != numero_secreto):
+        
+        print ("Cuidado! Você só tem mais", tentativas, "chance")
     
 
     chute_str = input ("\ndigite o seu número: ")
 
     chute = int (chute_str)
     acertou = chute == numero_secreto
-    
+
     maior = chute > numero_secreto
     menor = chute < numero_secreto
+    perdeu = tentativas == 0
 
     if (acertou):
+        tentativas = 0
+        vencedor = tentativas
         print ("Você acertou!\n")
         print ("Fim de jogo!\n")
     else:
+
         if (maior):
             tentativas -= 1
             print ("\nVocê errou! Seu chute foi um valor mais alto que o número secreto.\n")
+
         elif (menor):
+            
             tentativas -= 1
             print ("Você errou! Seu chute foi um valor mais baixo que o número secreto.\n")
 
-if (tentativas == 0):
-    print ("Suas tentativas acabaram.\n")
-    print ("Fim de jogo!\n")
+perdeu = tentativas == 0
 
+if(perdeu):
+    print("Suas chances acabaram. Você perdeu!")
