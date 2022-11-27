aef3f6fa7fc1dfcd684b32b7225cf85432d3e0c6
Pedro Itamaro <itamaropedro@gmail.com>
2022-11-26 22:05:16 -0300

    inicio jogo forca

--- forca.py
+++ forca.py
@@ -1,11 +1,23 @@
 def jogar():
-
-
     print("************************************")
     print("*****Bem vindo ao jogo da Forca*****")
     print("************************************")
 
-    print("Fim de jogo.")
+    palavra_secreta = "PYTHON"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual letra? ")
+        chute.isupper
+        index = 1
+        for letra in palavra_secreta:
+            if(chute==letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index += index
+    print("Fim de Jogo")
 
 if(__name__== "__main__"):
     jogar()
